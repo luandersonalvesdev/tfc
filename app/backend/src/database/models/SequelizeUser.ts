82d4a4da78c29d5663dea6827a34daf2f69a7c42
Luanderson Alves <luaoderson@gmail.com>
2023-08-31 18:08:55 -0300

    feat: migration, model and seed of User table

---
+++ app/backend/src/database/models/SequelizeUser.ts
@@ -0,0 +1,52 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+class SequelizeUser extends Model<InferAttributes<SequelizeUser>,
+InferCreationAttributes<SequelizeUser>> {
+  declare id: CreationOptional<number>;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+SequelizeUser.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    field: 'team_name',
+  },
+}, {
+  sequelize: db,
+  modelName: 'teams',
+  timestamps: false,
+});
+
+export default SequelizeUser;
