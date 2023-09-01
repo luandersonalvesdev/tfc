41e402ec33d927da078a189409d3f3d7b5b71520
Luanderson Alves <luaoderson@gmail.com>
2023-09-01 20:26:38 -0300

    feat: model, migration and seed of matches

---
+++ app/backend/src/database/models/SequelizeMatch.ts
@@ -0,0 +1,71 @@
+import {
+  DataTypes,
+  Model,
+  InferAttributes,
+  InferCreationAttributes,
+  CreationOptional,
+} from 'sequelize';
+import db from '.';
+
+import SequelizeTeam from './SequelizeTeam';
+
+class SequelizeMatch extends Model<InferAttributes<SequelizeMatch>,
+InferCreationAttributes<SequelizeMatch>> {
+  declare id: CreationOptional<number>;
+  declare homeTeamId: number;
+  declare homeTeamGoals: number;
+  declare awayTeamId: number;
+  declare awayTeamGoals: number;
+  declare inProgress: boolean;
+}
+
+SequelizeMatch.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  homeTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_id',
+  },
+  homeTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'home_team_goals',
+  },
+  awayTeamId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_id',
+  },
+  awayTeamGoals: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: 'away_team_goals',
+  },
+  inProgress: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+    field: 'in_progress',
+  },
+}, {
+  sequelize: db,
+  modelName: 'matches',
+  timestamps: false,
+});
+
+/**
+  * `Workaround` para aplicar as associations em TS:
+  * Associations 1:N devem ficar em uma das instâncias de modelo
+  * */
+
+SequelizeTeam.belongsTo(SequelizeMatch, { foreignKey: 'homeTeamId', as: 'homeMatches' });
+SequelizeTeam.belongsTo(SequelizeMatch, { foreignKey: 'awayTeamId', as: 'awayMatches' });
+
+SequelizeMatch.hasMany(SequelizeTeam, { foreignKey: 'homeTeamId', as: 'homeTeam' });
+SequelizeMatch.hasMany(SequelizeTeam, { foreignKey: 'awayTeamId', as: 'awayTeam' });
+
+export default SequelizeMatch;
